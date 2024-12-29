9e47d76c92d7b265e2da79107de0f0a59c77eb96
조다니엘(Daniel Cho) <arduinocc04@gmail.com>
2024-12-30 00:05:28 +0900

    Feat: 랭킹 시스템 구현중(미완성)
    - Firebase 연동하는 코드 Singleton으로 만드는 중
     - 데이터 받아오는 쪽이 제대로 안 됨

--- Assets/Stage/c#script/Rankingapi.cs
+++ Assets/Stage/c#script/Rankingapi.cs
@@ -7,159 +7,96 @@ using UnityEngine.Networking;
 using TMPro;
 using UnityEngine.UI;
 
+using Firebase;
+using Firebase.Database;
+using Firebase.Extensions;
+using System.Threading.Tasks;
+
 public class Rankingapi : MonoBehaviour
 {
-    public string[] onelistSplit;
-    public TextMeshProUGUI TextScore;
-    public TextMeshProUGUI TextUser;
-    public int Numbers = 2;
+    private Firebase.FirebaseApp app;
 
+    private Rankingapi() {}
 
-    // 요청 : Ranking Req     
-    public class ReqScoreRanking
+    private static Rankingapi _instance = null;
+
+    private void Awake()
     {
-        public string userId;
-        public int score;
+        if(_instance != null && _instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            var task = Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+                var dependencyStatus = task.Result;
+                Debug.Log(dependencyStatus);
+                if (dependencyStatus == Firebase.DependencyStatus.Available)
+                {
+                    app = Firebase.FirebaseApp.DefaultInstance;
+                }
+                else
+                {
+                    UnityEngine.Debug.LogError(System.String.Format(
+                    "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                }
+            });
+            _instance = this;
+        }
     }
 
-    public ReqScoreRanking cReqScoreRanking = null;
-
-    [System.Serializable]
-    public class ResRanking_Data
-    {
-        public string userId;
-        public int score;
-    }
-
-    [System.Serializable]
-    public class ResRanking_List
-    {
-        public List<ResRanking_Data> ranking;
-    }
-
-    public ResRanking_List cResRanking_List = null;
-
-    protected string strError;
+    public static Rankingapi Instance
+    {get{
+        return _instance;
+    }}
 
     // 랭킹등록
-    public void OnRequest_Api_Ranking_Req(string userId, int score)
-    {        
-       StartCoroutine(Send_Api_Ranking_Req(userId, score));
+    public void SendScore(string userId, int score)
+    {
+        var reference = FirebaseDatabase.DefaultInstance.RootReference;
+        reference.Child("scores").Child(userId).SetRawJsonValueAsync(score.ToString());
+    }
+
+    public void UpdateScore(string userId, int delta)
+    {
+        var reference = FirebaseDatabase.DefaultInstance.RootReference;
+        reference.Child("scores").Child(userId).GetValueAsync().ContinueWithOnMainThread(task => {
+            if(task.IsCompleted) {
+                if(task.Result == null)
+                {
+                    SendScore(userId, delta);
+                }
+                else
+                {
+                    int newScore = int.Parse(task.Result.GetRawJsonValue()) + delta;
+                    if(newScore < 0)
+                        newScore = 0;
+                    SendScore(userId, newScore);
+                }
+            }
+        });
     }
 
     // 랭킹조회 OnRequest_Api_Ranking_Req
-    public void OnRequest_Api_Ranking_List(string userId)
-    {        
-       StartCoroutine(Send_Api_Ranking_List(userId));
+    public async Task<DataSnapshot> GetScore(string userId)
+    {
+        var reference = FirebaseDatabase.DefaultInstance.RootReference;
+        var task = await reference.Child("scores").Child(userId).GetValueAsync();
+        return task;
     }
 
-    // 요청 상태 체크
-    public int CheckState_Request()
+    public async Task<DataSnapshot> GetTopScoreList(int n)
     {
-        return nState_Request;
+        var reference = FirebaseDatabase.DefaultInstance.RootReference;
+        var task = await reference.Child("scores").OrderByValue().LimitToLast(n).GetValueAsync();
+        return task;
     }
 
-    protected int nState_Request= 10; // 서버 요청 상태(0: 전송   1: 전송 성공   -1: 전송 실패   -2: 결과 파씽 에러)   
-
-    // ranking req
-    protected IEnumerator Send_Api_Ranking_Req(string userId, int score)
+    public object GetAllScoreList()
     {
-        ReqScoreRanking request = new ReqScoreRanking();
-        request.userId = userId;
-        request.score = score;
-
-        string strBody = JsonUtility.ToJson(request);
-
-        string url = "http://ec2-13-125-213-205.ap-northeast-2.compute.amazonaws.com:8080/api/v1/score/userinfo";
-
-        using (UnityWebRequest uwr = UnityWebRequest.Post(url, string.Empty))
-        {
-            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(strBody);
-            uwr.uploadHandler = new UploadHandlerRaw(jsonToSend);
-            uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-            uwr.SetRequestHeader("Content-Type", "application/json");
-            
-            nState_Request = 0;
-            yield return uwr.SendWebRequest();
-                        
-            if (uwr.isNetworkError || uwr.isHttpError)
-            {
-                nState_Request = -1;
-                strError = uwr.downloadHandler.text;
-            }
-            else
-            {
-                Debug.Log(uwr.downloadHandler.text);  
-                nState_Request = 1;
-            }
-            uwr.Dispose();
-        }
-    }
-
-    // Ranking List
-    protected IEnumerator Send_Api_Ranking_List(string userId)
-    {
-        ReqScoreRanking request = new ReqScoreRanking();
-        request.userId = userId;
-        request.score = 0;
-
-        string strBody = JsonUtility.ToJson(request);
-
-        string url = "http://ec2-13-125-213-205.ap-northeast-2.compute.amazonaws.com:8080/api/v1/list/userinfo";
-
-        using (UnityWebRequest uwr = UnityWebRequest.Post(url, string.Empty))
-        {
-            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(strBody);
-            uwr.uploadHandler = new UploadHandlerRaw(jsonToSend);
-            uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-            uwr.SetRequestHeader("Content-Type", "application/json");
-
-            nState_Request = 0;
-            // Debug.Log(strBody);  // {"userId":"aaa","score":0}
-            yield return uwr.SendWebRequest();
-                        
-            if (uwr.isNetworkError || uwr.isHttpError)
-            {
-                nState_Request = -1;
-                strError = uwr.downloadHandler.text;
-            }
-            else
-            {
-                Debug.Log(uwr.downloadHandler.text);//"ranking":[{"userId":"aaa","score":10},...
-                string OneList = uwr.downloadHandler.text;
-                string oneList = OneList.Replace("{\"ranking\":[{", "");
-                string onelist = oneList.Replace("}]}", "");
-
-                if (Numbers == 2){
-                    onelistSplit = onelist.Split("},{");
-                }else{
-                    string[] onelistSplit = onelist.Split(",");
-                    string calUser = onelistSplit[0];
-                    string[] calUserSplit = calUser.Split(":");
-
-                    string calscore = onelistSplit[1];
-                    string[] calscoreSplit = calscore.Split(":");
-
-                    Debug.Log(calUserSplit[1]);
-                    Debug.Log(calscoreSplit[1]);
-                    TextUser.text = calUserSplit[1]; //유저찍기
-                    TextScore.text = calscoreSplit[1]; //점수찍기
-                }
-
-                try
-                {
-                    cResRanking_List = JsonUtility.FromJson<ResRanking_List>(uwr.downloadHandler.text);
-                    nState_Request = 1;
-                }
-                catch (Exception _e)
-                {
-                    nState_Request = -2;
-                    Debug.Log(_e.Message);
-                }
-            }
-            uwr.Dispose();
-        }
+        var reference = FirebaseDatabase.DefaultInstance.RootReference;
+        var task = reference.Child("scores").OrderByValue().GetValueAsync();
+        task.Wait();
+        return task.Result.GetValue(true);
     }
 }
-
-// 주소   http://43.201.111.31:8080/swagger-ui.html
