82bfa8f74aa0eeec82754b00b9652fc771c66394
조다니엘(Daniel Cho) <arduinocc04@gmail.com>
2024-12-29 22:27:46 +0900

    Feat: 랭킹 전투 제작
    - Stage3에서 훔쳐옴

---
+++ Assets/RankingStage/c#script/RankingFightBarScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankingFightBarScript : MonoBehaviour
+{
+    public int fighthbar;
+    bool once;
+    
+    public GameObject fighthbarObject;
+
+    void Start()
+    {
+        once = false;
+        fighthbar = 5;
+        fighthbarObject.GetComponent<Slider>().value = 5;
+    }
+
+    void Update() 
+    {
+        if(true || GameObject.Find("NumberBundle").GetComponent<RankingNumberBundleScript>().going == 1)
+            fighthbarObject.GetComponent<Slider>().value = fighthbar;
+
+        if (fighthbarObject.GetComponent<Slider>().value >= 10 && once == false)
+        {
+            GameObject.Find("Stage").GetComponent<RankingStage>().Win();
+            once = true;
+        }
+
+        if (fighthbarObject.GetComponent<Slider>().value <= 0 && once == false)
+        {
+            GameObject.Find("Stage").GetComponent<RankingStage>().Lose();
+            once = true;
+        }
+    }
+}
